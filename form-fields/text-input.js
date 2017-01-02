10049b1d7f43bfed5682370c90f1860f49497593
Cunningham, Nick <nick_cunningham@intuit.com>
2017-01-01 23:57:23 -0800

    remove colon

--- form-fields/text-input.js
+++ form-fields/text-input.js
@@ -16,7 +16,7 @@ export class TextInput extends React.Component {
 
   render() {
     return  <div className="form-field">
-              <label>{this.props.label}: <br />
+              <label>{this.props.label}
                 <input type={this.props.type} className="form-control" name={this.props.name} onChange={this.props.onChange}/>
               </label>
             </div>
